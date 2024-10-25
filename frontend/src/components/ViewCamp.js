4bd33cd5565b2c399dc19e0b26fd7ac15805cdba
swill93 <swill140@emich.edu>
2024-10-25 13:08:49 -0400

    Added DetailsPage.css and Modified ViewCamp.js

--- frontend/src/components/ViewCamp.js
+++ frontend/src/components/ViewCamp.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
 import '../App.css';
+import '../DetailsPage.css';
 
 const CampView = () => {
   const { id } = useParams(); // Get the campground ID from the URL
@@ -18,8 +19,16 @@ const CampView = () => {
   }, [id]);
 
   return (
-    <div className="camp-container">
+    <div className="view-camp-container">
       {camp ? (
+        <>
+        {/* Section for Map and Camp Details */}
+        <div className = "map-camp-section" >
+        <div className = "map-container">
+          <div className = "map-placeholder">
+          </div>
+          </div>
+
         <div className="camp-details">
           <h1>{camp.name}</h1>
           <p><strong>Location:</strong> {camp.latitude}, {camp.longitude}</p>
@@ -31,6 +40,26 @@ const CampView = () => {
           <p><strong>Dates Open:</strong> {camp.datesOpen || 'N/A'}</p>
           <Link to={"/"}>Back</Link>
         </div>
+        </div>
+
+        {/* Review Section */}
+        <div className = "review-section">
+          <h2> Leave a Review </h2>
+          <textarea placeholder = "Leave a review..." />
+          <button> Submit </button>
+        </div>
+
+        {/* Similar Campgrounds */}
+        <div className = "similar-campgrounds">
+          <h2> Similar Campgrounds </h2>
+          <div className = "campgrounds">
+            <div className = "camping-card">Campground 1</div>
+            <div className = "camping-card">Campground 2</div>
+            <div className = "camping-card">Campground 3</div>
+            <div className = "camping-card">Campground 4</div>
+          </div>
+        </div>
+        </>
       ) : (
         <p>Loading camp details...</p>
       )}
