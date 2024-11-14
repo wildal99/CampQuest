79b198451b4855e016b30bd0be14ca90ec326046
William Dalian <williamdalian1@gmail.com>
2024-11-14 08:51:46 -0500

    Start adding similar camp finding logic
    FEAT: [ViewCamps.js] display similar camps on each campground page.
    
    Add useEffect to get list of all campgrounds from the database in ViewCamp.
    
    Add findSimilarity function  to return a list of the similarity of campgrounds
    
    Add computeDistance function to find the distance between campgrounds.
    
    add findRadians function to enable computing distance between campgrounds.
    
    IN PROGRESS for issue number 21

--- frontend/src/components/ViewCamp.js
+++ frontend/src/components/ViewCamp.js
@@ -5,8 +5,12 @@ import '../App.css';
 import '../DetailsPage.css';
 
 const CampView = () => {
+  console.log("hello from campview.")
+
   const { id } = useParams(); // Get the campground ID from the URL
   const [camp, setCamp] = useState(null);
+  const [similar, setSimilar] = useState([]);
+  const [campsList, setCampList] = useState([]);
 
   useEffect(() => {
     axios.get(`http://localhost:5000/camps/${id}`)
@@ -18,7 +22,20 @@ const CampView = () => {
       });
   }, [id]);
 
-  return (
+  useEffect(() => {
+    axios.get('http://localhost:5000/camps/')
+      .then(response => {
+        setCampList(response.data);
+      })
+      .catch((error) => {
+        console.log('Error fetching similar campgrounds:', error);
+      });
+  }, []);  
+
+console.log(campsList);
+
+
+return(
     <div className="view-camp-container">
       {camp ? (
         <>
@@ -49,23 +66,50 @@ const CampView = () => {
           <button> Submit </button>
         </div>
 
+
         {/* Similar Campgrounds */}
         <div className = "similar-campgrounds">
           <h2> Similar Campgrounds </h2>
           <div className = "campgrounds">
-            <div className = "camping-card">Campground 1</div>
+
+
+            <div className = "camping-card"></div>
             <div className = "camping-card">Campground 2</div>
             <div className = "camping-card">Campground 3</div>
             <div className = "camping-card">Campground 4</div>
           </div>
-        </div>
-        </>
+        </div> 
+        </> 
       ) : (
         <p>Loading camp details...</p>
       )}
+      
     </div>
-  );
-  
-}
+    );
+
+  }
 
 export default CampView;
+
+function findSimilarity(self, others){
+  return others.map( othr=> {
+    const distance = computeDistance(self.latitude, self.longitude, othr.latitude, othr.longitude);
+    return {campgroundName: othr.campgroundName, campgroundCode: othr.campgroundCode,longitude: 
+      othr.longitude, latitude: othr.latitude, phoneNumber: othr.phoneNumber, campgroundType: othr.campgroundType,
+      numSites: othr.numSites, datesOpen: othr.datesOpen, similarity: distance}
+  });
+}
+
+function computeDistance(latCurr, longCurr, latCamp, longCamp){
+  let dist= Math.acos((Math.sin(findRadians(latCurr)) * Math.sin(findRadians(latCamp))) + (Math.cos(findRadians(latCurr)) * Math.cos(findRadians(latCamp))) * (Math.cos(findRadians(longCamp) - findRadians(longCurr)))) * 6371;
+
+  return dist
+}
+
+
+// convert degrees to radians.
+function findRadians(degrees)
+{
+  return degrees * (Math.pi/180);
+}
+
