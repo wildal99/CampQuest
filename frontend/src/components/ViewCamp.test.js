65bb619f7a78fec85d01c8a7b02f349d09f16b3d
William Dalian <williamdalian1@gmail.com>
2024-12-07 14:21:04 -0500

    Add initial tests for ViewCamp
    Add test to ensure viewcamp displays Loding campground details until campground details are loaded.

--- frontend/src/components/ViewCamp.test.js
+++ frontend/src/components/ViewCamp.test.js
@@ -1,4 +1,5 @@
 import { render, screen, waitFor} from '@testing-library/react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ViewCamp from './ViewCamp';
 
 test('ViewCamp should display loading campground information until the campground data is loaded', () => {
@@ -6,4 +7,3 @@ test('ViewCamp should display loading campground information until the campgroun
 
     const loadingText = screen.getByText("Loading camp details...")
 });
-
